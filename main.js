9b9692426ace29693190dd0ff1463cf9c5bb8ba1
setorailhomjonova0708 <setorailhomjonova@gmail.com>
2026-05-14 01:32:43 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("changeTextBtn");
+
+button.addEventListener("click", () => {
+    button.innerText = "Rahmat 😊";
+    alert("Portfolio saytga xush kelibsiz!");
+});
